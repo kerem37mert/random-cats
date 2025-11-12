f33603b633cee95abd512097b83cf9b3737574bb
kerem37mert <kerem37mert@yahoo.com>
2025-11-12 11:24:16 +0300

    favCats updated

--- components/FavCard/FavCard.js
+++ components/FavCard/FavCard.js
@@ -1,10 +1,12 @@
 import classes from "./FavCard.module.css";
 import Image from "next/image";
+import { MdDelete } from "react-icons/md";
 
-export default function FavCard({ id }) {
+export default function FavCard({ id, onRemove }) {
     return (
         <div className={ classes["fav-card"] }>
             <Image src={ `https://cdn2.thecatapi.com/images/${id}.jpg` } alt="keddy" width={ 300 } height={ 300 } />
+            <MdDelete className={ classes.remove } onClick={ () => onRemove(id) } />
         </div>
     );
 }
--- components/FavCardContainer/FavCardContainer.js
+++ components/FavCardContainer/FavCardContainer.js
@@ -8,12 +8,16 @@ export default function FavCardContainer() {
 
     const { value, setValue } = useLocalStorage("favs", []);
 
+    function removeFav(id) {
+        setValue(prev => prev.filter(item => item !== id));
+    }
+
     return(
         <div className={ classes["fav-card-container"] }>
             { 
                 value.length ? (
                     value.map(item => (
-                        <FavCard key={ item } id={ item } />
+                        <FavCard key={ item } id={ item } onRemove={ removeFav } />
                     )) 
                 ) : (
                     <div className={ classes["not-found"] }>

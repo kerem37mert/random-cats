fe08f97c9fcee744331a3e7d9cb85b1a7c11ebee
kerem37mert <kerem37mert@yahoo.com>
2025-12-14 19:30:58 +0300

    image changed

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,27 +1,33 @@
 import Link from "next/link";
 import classes from "./Header.module.css";
-import logoImg from "@/assets/logo.png"
+import logoImg from "@/assets/logo.png";
 import Image from "next/image";
 
 export default function Header() {
-    return (
-        <header className={ classes.header }>
-            <Link href="/" className={ classes.logo }>
-                Happy Kitty
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        Soft Kitty
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/" className={classes.link}>
+              Anasayfa
             </Link>
-            <nav className={ classes.nav }>
-                <ul>
-                    <li>
-                        <Link href="/" className={ classes.link }>Anasayfa</Link>
-                    </li>
-                    <li>
-                        <Link href="/favs" className={ classes.link }>Favori Kediler</Link>
-                    </li>
-                    <li>
-                        <Link href="/about" className={ classes.link }>Hakkımızda</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+          </li>
+          <li>
+            <Link href="/favs" className={classes.link}>
+              Favori Kediler
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className={classes.link}>
+              Hakkımızda
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
